bdcf05116fe53271c43767a779d98f655b1bd4da
lin <1248075286@qq.com>
2019-01-30 19:31:07 +0800

    feature static page

---
+++ precache-manifest.333be5cfdbdc737effbb14c5d37c7a66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "24374018bd68b55666aaaf1800d55de4",
+    "url": "/static/media/type.24374018.svg"
+  },
+  {
+    "revision": "9fffe4b109bd5c2e57ea56b879d00954",
+    "url": "/static/media/read.9fffe4b1.svg"
+  },
+  {
+    "revision": "df66e0c1c364b02064e6fd483a330f3b",
+    "url": "/static/media/name.df66e0c1.svg"
+  },
+  {
+    "revision": "48df608f8e5d77717c91b844c35e7114",
+    "url": "/static/media/book.48df608f.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a01105c9ecb2d83bdd5a",
+    "url": "/static/js/main.a01105c9.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "a01105c9ecb2d83bdd5a",
+    "url": "/static/css/main.8d4151df.chunk.css"
+  },
+  {
+    "revision": "b855ecc3b5bb42c3c253fb0d18839aae",
+    "url": "/index.html"
+  }
+];
