35de8a05c832e6879f80bf800022801686dec0fa
graybin <lgraybin@163.com>
2019-04-11 11:59:14 +0800

    server

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ var app = express();
 var blogList = require('./list/index.js')
 app.get('/',(req, res)=>{
 	console.log('send index.html')
-	res.set({'Access-Control-Allow-Origin': 'linzhenbin.com'})
+	res.set({'Access-Control-Allow-Origin': 'http://linzhenbin.com'})
 	res.sendFile(__dirname + '/index.html')
 })
 app.use('/static',(req, res) => {
-	res.set({'Access-Control-Allow-Origin': 'linzhenbin.com'})
+	res.set({'Access-Control-Allow-Origin': 'http://linzhenbin.com'})
 	res.sendFile(__dirname + '/static' + req.path);
 })
 app.use('/list', (req, res) => {
@@ -16,11 +16,11 @@ app.use('/list', (req, res) => {
 	pageList.data = []
 	if(req.query.page){
 		let page = req.query.page
-		res.set({'Access-Control-Allow-Origin': 'linzhenbin.com'})
+		res.set({'Access-Control-Allow-Origin': 'http://linzhenbin.com'})
 		pageList.data = blogList.data.slice( (page- 1)*5, page*5)
 	}else{
 		let type = req.query.type
-		res.set({'Access-Control-Allow-Origin': 'linzhenbin.com'})
+		res.set({'Access-Control-Allow-Origin': 'http://linzhenbin.com'})
 		blogList.data.forEach((item)=>{
 			if(item.type === type){
 				pageList.data.push(item)
@@ -30,7 +30,7 @@ app.use('/list', (req, res) => {
 	res.json(pageList)
 })
 app.use('/content',(req, res) =>{
-	res.set({'Access-Control-Allow-Origin': 'linzhenbin.com'})
+	res.set({'Access-Control-Allow-Origin': 'http://linzhenbin.com'})
 	res.sendFile(__dirname + '/public/md_files' + req.path);
 })
 app.listen(80,()=>{
